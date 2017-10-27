b56d2327c81d1005118b1a71fc853c0008d37b74
unknown <tsdcanavan@yahoo.com>
2017-10-26 20:22:09 -0400

    formatting, fontawesome

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var bodyParser = require("body-parser");
 var path = require('path');
 
 // survey list
-var stmnt = [];
-stmnt[0] = 'I like sci-fi movies';
-stmnt[1] = 'I like playing sports';
-stmnt[2] = 'I like watching sports';
-stmnt[3] = 'I like eating out';
-stmnt[4] = 'I like staying home';
-stmnt[5] = 'I like this class',
-stmnt[6] = 'I hate this class';
-stmnt[7] = 'I like music';
-stmnt[8] = 'I miss Jeremiah';
-stmnt[9] = 'I like this survey';
+// var stmnt = [];
+// stmnt[0] = 'I like sci-fi movies';
+// stmnt[1] = 'I like playing sports';
+// stmnt[2] = 'I like watching sports';
+// stmnt[3] = 'I like eating out';
+// stmnt[4] = 'I like staying home';
+// stmnt[5] = 'I like this class',
+// stmnt[6] = 'I hate this class';
+// stmnt[7] = 'I like music';
+// stmnt[8] = 'I miss Jeremiah';
+// stmnt[9] = 'I like this survey';
 
 // Create express app instance.
 var app = express();
